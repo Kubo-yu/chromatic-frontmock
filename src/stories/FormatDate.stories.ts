ab56eafb214e5cfc8ff08d97ad598cc712704ea3
Kubo-yu <dev.2nd.yk@gmail.com>
2023-05-09 15:18:07 +0900

    Dateを固定

--- src/stories/FormatDate.stories.ts
+++ src/stories/FormatDate.stories.ts
@@ -7,7 +7,7 @@ const meta: Meta<typeof FormatDate> = {
   tags: ['autodocs'],
 };
 
-const today = new Date()
+const date = new Date("2023/01/01")
 
 export default meta;
 type Story = StoryObj<typeof FormatDate>;
@@ -15,27 +15,27 @@ type Story = StoryObj<typeof FormatDate>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Hyphen: Story = {
   args: {
-    date: today,
+    date: date,
     format: 1
   },
 };
 export const HyphenWithTime: Story = {
   args: {
-    date: today,
+    date: date,
     format: 2
   },
 };
 
 export const Slash: Story = {
   args: {
-    date: today,
+    date: date,
     format: 3
   },
 };
 
 export const SlashWithDay: Story = {
   args: {
-    date: today,
+    date: date,
     format: 4
   },
 };
