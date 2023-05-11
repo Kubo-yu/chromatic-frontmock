01ea3606d4e7ee024f2951aa07d0edfd650cb66d
Kubo-yu <dev.2nd.yk@gmail.com>
2023-05-11 14:01:26 +0900

    sb viewportテスト

--- src/stories/DialogButton.stories.tsx
+++ src/stories/DialogButton.stories.tsx
@@ -2,6 +2,8 @@ import type { Meta, StoryObj } from '@storybook/react';
 import {DialogButton} from '../components/chromatic-test/DialogButton';
 import { within, userEvent } from '@storybook/testing-library';
 
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
 const meta: Meta<typeof DialogButton> = {
   title: 'Components/DialogButton',
   component: DialogButton,
@@ -12,14 +14,41 @@ export default meta;
 type Story = StoryObj<typeof DialogButton>;
 
 export const Default: Story = {
+  // chromatic viewport 設定
+  // 設定しているのは画面幅、これによって、スマホやタブレットの指定の画面幅でテストできる。
+  parameters: {
+    chromatic: {
+      viewport: [1200, 390]
+    },
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+      defaultViewPort: "iPohne13"
+    }
+  }
 };
 
 export const IsOpend: Story = {
+  // ボタンをクリックした時の挙動を追加
   play: async({canvasElement}) => {
+    // `canvasElement`内で操作を行うためのcanvasオブジェクトを作成。
     const canvas = within(canvasElement);
-    const button = await canvas.findByText("ダイアログを開く")
 
-    await userEvent.click(button)
+    // "ダイアログを開く"というテキストを含むボタンを探す。
+    const button = await canvas.findByText("ダイアログを開く");
+    
+    // ボタンをクリック。
+    await userEvent.click(button);
+  },
+  // chromatic viewport 設定
+  // 設定しているのは画面幅、これによって、スマホやタブレットの指定の画面幅でテストできる。
+  parameters: {
+    chromatic: {
+      viewport: [1200, 390]
+    },
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+      defaultViewPort: "iPohne13"
+    }
   }
 };
 
