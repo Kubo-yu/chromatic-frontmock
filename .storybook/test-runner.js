353a46176d460413eb83386cea4b9f0116e67bf6
Kubo-yu <dev.2nd.yk@gmail.com>
2023-05-15 10:07:49 +0900

    アクセシビリティテスト

--- .storybook/test-runner.js
+++ .storybook/test-runner.js
@@ -1,15 +1,38 @@
-const  {injectAxe, checkA11y} = require('axe-playwright');
+const  {injectAxe, checkA11y, getViolations} = require('axe-playwright');
 
 module.exports = {
+  setup() {
+    expect.extend({
+      toValidA11y(violations) {
+        if(violations.length === 0) {
+          return {pass: true};
+        }
+        return {
+          message: () => 
+            'アクセシビリティエラーが見つかりました。\n' + 
+            JSON.stringfy(
+              violations.map(({id, imapct, description, help}) => ({
+                id, imapct, description, help,
+              })), 
+              '\n',
+              ' '
+            ),
+            pass: false
+        }
+      }
+    })
+  },
   async preRender(page) {
     await injectAxe(page);
   },
   async postRnder(page) {
-    await checkA11y(page, '#storybook-root', {
-      detailedReport: true,
-      detailedReportOptions: {
-        html: true,
-      }
-    })
+    const violations = await getViolations(page, '#storybook-root');
+    expect(violations).toValidA11y()
+    // await checkA11y(page, '#storybook-root', {
+    //   detailedReport: true,
+    //   detailedReportOptions: {
+    //     html: true,
+    //   }
+    // })
   }
 }
